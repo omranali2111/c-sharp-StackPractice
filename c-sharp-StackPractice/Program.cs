07448962976988ad28698a32d4de2b8944503e76
omranali2111 <aljabri1234@hotmail.com>
2023-07-16 12:41:22 +0400

    checking if the parentheses order is balanced

---
+++ c-sharp-StackPractice/Program.cs
@@ -0,0 +1,46 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+       Console.WriteLine($"the parentheses order is: {isInOrder("){})")}");
+    }
+    
+    public static bool isInOrder(string input)
+    {
+        
+        Stack<char> stack = new Stack<char>();
+        for (int i = 0; i < input.Length; i++)
+        {
+            char character = input[i];
+            if (character == '[' || character == '{' || character == '(')
+                stack.Push(character);
+            else if (character == ']' || character == '}' || character == ')')
+            {
+                if (!stack.Any()) 
+                    return false;
+                switch (character)
+                {
+                    case ']':
+                        if (stack.Pop() != '[')
+                            return false;
+                        break;
+                    case '}':
+                        if (stack.Pop() != '{')
+                            return false;
+                        break;
+                    case ')':
+                        if (stack.Pop() != '(')
+                            return false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+       
+        if (!stack.Any())
+            return true;
+        return false;
+    }
+}
+
